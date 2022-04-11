a8691c6367254dbd33f96291820f6d1d87198773
puntonugroho <punugroho@gmail.com>
2022-04-11 12:56:44 +0700

    changes in db and font

---
+++ application/logs/log-2022-04-11.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-04-11 07:53:48 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) C:\xampp\htdocs\amsal2\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2022-04-11 07:53:48 --> Unable to connect to the database
+ERROR - 2022-04-11 07:53:50 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) C:\xampp\htdocs\amsal2\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2022-04-11 07:53:50 --> Unable to connect to the database
+ERROR - 2022-04-11 07:53:50 --> Query error: Access denied for user 'root'@'localhost' (using password: NO) - Invalid query: SELECT *
+FROM `sys_user_online`
+WHERE `id` = 70
+ERROR - 2022-04-11 07:53:50 --> Severity: Error --> Call to a member function row() on a non-object C:\xampp\htdocs\amsal2\application\controllers\HalamanUtama.php 12
+ERROR - 2022-04-11 07:54:46 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\amsal2\application\views\header.php 92
+ERROR - 2022-04-11 07:54:59 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\amsal2\application\views\header.php 92
