1762b65a61500ce0dc1166746e7dba6ea9ba7d84
puntonugroho <punugroho@gmail.com>
2022-04-11 16:07:41 +0700

    log

--- application/logs/log-2022-04-11.php
+++ application/logs/log-2022-04-11.php
@@ -19,3 +19,28 @@ ERROR - 2022-04-11 08:26:55 --> 404 Page Not Found: Assets/maps
 ERROR - 2022-04-11 08:26:55 --> 404 Page Not Found: Assets/js
 ERROR - 2022-04-11 08:33:46 --> 404 Page Not Found: Assets/js
 ERROR - 2022-04-11 08:33:46 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 10:58:08 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 10:58:08 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 10:58:15 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 10:58:15 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 10:59:09 --> 404 Page Not Found: Dokumen/4030df0ca5271db1ddd36c0bdc42361a.pdf
+ERROR - 2022-04-11 10:59:44 --> 404 Page Not Found: Dokumen/4030df0ca5271db1ddd36c0bdc42361a.pdf
+ERROR - 2022-04-11 10:59:51 --> 404 Page Not Found: Dokumen/e398a4f7c61413c8d8e6fb98c3edca43.pdf
+ERROR - 2022-04-11 11:00:06 --> 404 Page Not Found: Dokumen/4030df0ca5271db1ddd36c0bdc42361a.pdf
+ERROR - 2022-04-11 11:00:41 --> 404 Page Not Found: Dokumen/4030df0ca5271db1ddd36c0bdc42361a.pdf
+ERROR - 2022-04-11 11:00:45 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 11:00:45 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 11:00:48 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 11:00:48 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 11:03:07 --> The upload path does not appear to be valid.
+ERROR - 2022-04-11 11:03:07 --> Query error: Duplicate entry '0-1' for key 'PRIMARY' - Invalid query: INSERT INTO `register_surat` (`klasifikasi_surat_id`, `klasifikasi_surat`, `nomor_index`, `nomor_agenda`, `jenis_surat_id`, `jenis_surat`, `tanggal_register`, `tanggal_surat`, `nomor_surat`, `pengirim`, `tujuan_id`, `tujuan`, `perihal`, `dokumen_elektronik`, `keterangan`, `status_pelaksanaan_id`, `status_pelaksanaan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('1', 'Surat Masuk', '12', '12/2022', '2', 'Penting', '2022-04-11', '2022-04-11', '2323', 'affa', '2', 'Ketua Mahkamah Syar\'iyah', 'fafadfad', NULL, 'fadf', '1', 'Pendaftaran', 'admin', '2022-04-11 11:03:07')
+ERROR - 2022-04-11 11:04:14 --> The upload path does not appear to be valid.
+ERROR - 2022-04-11 11:04:14 --> Query error: Duplicate entry '0-1' for key 'PRIMARY' - Invalid query: INSERT INTO `register_surat` (`klasifikasi_surat_id`, `klasifikasi_surat`, `nomor_index`, `nomor_agenda`, `jenis_surat_id`, `jenis_surat`, `tanggal_register`, `tanggal_surat`, `nomor_surat`, `pengirim`, `tujuan_id`, `tujuan`, `perihal`, `dokumen_elektronik`, `keterangan`, `status_pelaksanaan_id`, `status_pelaksanaan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('1', 'Surat Masuk', '12', '12/2022', '1', 'Rahasia', '2022-04-11', '2022-04-11', 'W1-A22/11', 'fsdffsdf', '2', 'Ketua Mahkamah Syar\'iyah', 'sdfdf', NULL, 'sdfsdf', '1', 'Pendaftaran', 'admin', '2022-04-11 11:04:14')
+ERROR - 2022-04-11 11:04:28 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 11:04:28 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 11:04:34 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 11:04:34 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 11:05:12 --> The upload path does not appear to be valid.
+ERROR - 2022-04-11 11:05:12 --> Query error: Duplicate entry '0-1' for key 'PRIMARY' - Invalid query: INSERT INTO `register_surat` (`klasifikasi_surat_id`, `klasifikasi_surat`, `nomor_index`, `nomor_agenda`, `jenis_surat_id`, `jenis_surat`, `tanggal_register`, `tanggal_surat`, `nomor_surat`, `pengirim`, `tujuan_id`, `tujuan`, `perihal`, `dokumen_elektronik`, `keterangan`, `status_pelaksanaan_id`, `status_pelaksanaan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('1', 'Surat Masuk', '12', '12/2022', '1', 'Rahasia', '2022-04-11', '2022-04-11', 'W1-A22/11/220', 'kdmflkdfmda', '2', 'Ketua Mahkamah Syar\'iyah', 'dfdfdsfsd', NULL, 'dsfsdfsdf', '1', 'Pendaftaran', 'admin', '2022-04-11 11:05:12')
+ERROR - 2022-04-11 11:05:15 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 11:05:15 --> 404 Page Not Found: Assets/js
