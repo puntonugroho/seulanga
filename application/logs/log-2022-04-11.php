45589f5c57cee7a46579a3ca160501a2a34f994f
puntonugroho <punugroho@gmail.com>
2022-04-11 13:41:53 +0700

    icon

--- application/logs/log-2022-04-11.php
+++ application/logs/log-2022-04-11.php
@@ -10,3 +10,12 @@ WHERE `id` = 70
 ERROR - 2022-04-11 07:53:50 --> Severity: Error --> Call to a member function row() on a non-object C:\xampp\htdocs\amsal2\application\controllers\HalamanUtama.php 12
 ERROR - 2022-04-11 07:54:46 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\amsal2\application\views\header.php 92
 ERROR - 2022-04-11 07:54:59 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\amsal2\application\views\header.php 92
+ERROR - 2022-04-11 07:59:14 --> Severity: Warning --> in_array() expects parameter 2 to be array, null given C:\xampp\htdocs\amsal2\application\views\header.php 92
+ERROR - 2022-04-11 08:09:54 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 08:09:54 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 08:26:50 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 08:26:50 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 08:26:55 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-11 08:26:55 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 08:33:46 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-11 08:33:46 --> 404 Page Not Found: Assets/maps
