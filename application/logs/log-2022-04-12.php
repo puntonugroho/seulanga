6802910c185d75b88296942a6a44df33ff7c7ea1
Ibrahim A. Hrp <aim10hrp@gmail.com>
2022-04-12 12:39:53 +0700

    db

--- application/logs/log-2022-04-12.php
+++ application/logs/log-2022-04-12.php
@@ -16,3 +16,13 @@ ERROR - 2022-04-12 04:45:28 --> 404 Page Not Found: Assets/maps
 ERROR - 2022-04-12 04:45:29 --> 404 Page Not Found: Assets/js
 ERROR - 2022-04-12 04:48:41 --> Query error: Duplicate entry '0-17' for key 'PRIMARY' - Invalid query: INSERT INTO `register_pelaksanaan` (`register_id`, `jenis_pelaksanaan_id`, `jenis_pelaksanaan`, `tanggal_pelaksanaan`, `dari_userid`, `dari_fullname`, `dari_jabatan_id`, `dari_jabatan`, `kepada_userid`, `kepada_fullname`, `kepada_jabatan_id`, `kepada_jabatan`, `keterangan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('17', '10', 'Disposisi', '2022-04-12', '2', 'Dra. Hj. Rosmawardani, S.H., M.H', '2', 'Ketua Mahkamah Syar\'iyah', '2', 'Drs. Syafruddin', '4', 'Panitera', '', 'ros', '2022-04-12 04:48:41')
 ERROR - 2022-04-12 04:56:58 --> Query error: Duplicate entry '0-17' for key 'PRIMARY' - Invalid query: INSERT INTO `register_pelaksanaan` (`register_id`, `jenis_pelaksanaan_id`, `jenis_pelaksanaan`, `tanggal_pelaksanaan`, `dari_userid`, `dari_fullname`, `dari_jabatan_id`, `dari_jabatan`, `kepada_userid`, `kepada_fullname`, `kepada_jabatan_id`, `kepada_jabatan`, `keterangan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('17', '10', 'Disposisi', '2022-04-12', '2', 'Dra. Hj. Rosmawardani, S.H., M.H', '2', 'Ketua Mahkamah Syar\'iyah', '7', 'Drs. Ilyas, S.H., M.H', '7', 'Panitera Muda Hukum', '', 'ros', '2022-04-12 04:56:58')
+ERROR - 2022-04-12 05:42:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\amsal\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2022-04-12 05:42:37 --> Unable to connect to the database
+ERROR - 2022-04-12 05:42:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\amsal\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2022-04-12 05:42:37 --> Unable to connect to the database
+ERROR - 2022-04-12 05:42:37 --> Query error: Access denied for user 'root'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `sys_user_online`
+WHERE `id` = 182
+ERROR - 2022-04-12 05:42:37 --> Severity: Error --> Call to a member function row() on boolean C:\xampp\htdocs\amsal\application\controllers\HalamanUtama.php 18
+ERROR - 2022-04-12 07:31:42 --> 404 Page Not Found: Dokumen/e398a4f7c61413c8d8e6fb98c3edca43.pdf
+ERROR - 2022-04-12 07:31:42 --> 404 Page Not Found: Dokumen/e398a4f7c61413c8d8e6fb98c3edca43.pdf
