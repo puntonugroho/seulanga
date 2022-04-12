46296a82cf6bceaf83e1a3ad7e0481d918f7bcf5
puntonugroho <punugroho@gmail.com>
2022-04-12 13:03:18 +0700

    upload file size

--- application/logs/log-2022-04-12.php
+++ application/logs/log-2022-04-12.php
@@ -16,3 +16,47 @@ ERROR - 2022-04-12 04:45:28 --> 404 Page Not Found: Assets/maps
 ERROR - 2022-04-12 04:45:29 --> 404 Page Not Found: Assets/js
 ERROR - 2022-04-12 04:48:41 --> Query error: Duplicate entry '0-17' for key 'PRIMARY' - Invalid query: INSERT INTO `register_pelaksanaan` (`register_id`, `jenis_pelaksanaan_id`, `jenis_pelaksanaan`, `tanggal_pelaksanaan`, `dari_userid`, `dari_fullname`, `dari_jabatan_id`, `dari_jabatan`, `kepada_userid`, `kepada_fullname`, `kepada_jabatan_id`, `kepada_jabatan`, `keterangan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('17', '10', 'Disposisi', '2022-04-12', '2', 'Dra. Hj. Rosmawardani, S.H., M.H', '2', 'Ketua Mahkamah Syar\'iyah', '2', 'Drs. Syafruddin', '4', 'Panitera', '', 'ros', '2022-04-12 04:48:41')
 ERROR - 2022-04-12 04:56:58 --> Query error: Duplicate entry '0-17' for key 'PRIMARY' - Invalid query: INSERT INTO `register_pelaksanaan` (`register_id`, `jenis_pelaksanaan_id`, `jenis_pelaksanaan`, `tanggal_pelaksanaan`, `dari_userid`, `dari_fullname`, `dari_jabatan_id`, `dari_jabatan`, `kepada_userid`, `kepada_fullname`, `kepada_jabatan_id`, `kepada_jabatan`, `keterangan`, `diinput_oleh`, `diinput_tanggal`) VALUES ('17', '10', 'Disposisi', '2022-04-12', '2', 'Dra. Hj. Rosmawardani, S.H., M.H', '2', 'Ketua Mahkamah Syar\'iyah', '7', 'Drs. Ilyas, S.H., M.H', '7', 'Panitera Muda Hukum', '', 'ros', '2022-04-12 04:56:58')
+ERROR - 2022-04-12 05:57:52 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 05:58:06 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 05:59:34 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:00:46 --> Severity: Parsing Error --> syntax error, unexpected '$queryLogin' (T_VARIABLE), expecting ')' C:\xampp\htdocs\amsal\application\controllers\HalamanLogin.php 68
+ERROR - 2022-04-12 06:00:54 --> Severity: Parsing Error --> syntax error, unexpected '$queryLogin' (T_VARIABLE) C:\xampp\htdocs\amsal\application\controllers\HalamanLogin.php 68
+ERROR - 2022-04-12 06:01:11 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:01:23 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:02:14 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:03:05 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:03:20 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:05:52 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:06:04 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:10:49 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:11:03 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:11:56 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:12:09 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:19:12 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-12 06:19:12 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:19:19 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-12 06:19:19 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:19:28 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:19:29 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-12 06:24:38 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:24:51 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:25:01 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:25:14 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:25:29 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:25:55 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:26:08 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:34:25 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:38:28 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:38:40 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:40:20 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:40:27 --> Severity: Error --> Call to a member function result() on a non-object C:\xampp\htdocs\amsal\application\views\halamanutama\index.php 84
+ERROR - 2022-04-12 06:41:01 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:44:54 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:45:07 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:50:08 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 06:50:12 --> Severity: Error --> Call to a member function result() on a non-object C:\xampp\htdocs\amsal\application\views\halamanutama\index.php 84
+ERROR - 2022-04-12 06:51:01 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 07:17:20 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 07:17:20 --> 404 Page Not Found: Assets/js
+ERROR - 2022-04-12 07:17:26 --> 404 Page Not Found: Assets/maps
+ERROR - 2022-04-12 07:17:26 --> 404 Page Not Found: Assets/js
